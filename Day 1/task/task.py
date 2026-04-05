9a77602ca1cf77de887f407b5dead812d20ddb4b
TechnoOrc <technoorc@gmail.com>
2026-04-06 05:46:19 +0900

    # Day 1 print() input() len() variable

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,4 @@
+# print() input() valiable
 city_name = input("Welcome to the Band Name Generator.\nWhat's the name of the city yoou grew up in?\n")
 pet_name = input("What's your pet's name?\n")
 print("Your band name could be " + city_name +  " " + pet_name)

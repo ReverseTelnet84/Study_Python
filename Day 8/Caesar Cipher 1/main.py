1c9a70a4d63142899e6c5fb0230be775cbc884a3
TechnoOrc <technoorc@gmail.com>
2026-06-05 06:07:30 +0900

    Day8 : Functions with Inputs
    #inputs
    #Arguments
    #Parameters
    
    #Love Calculator
    #Caesar Cipher 1
    
    List index() Method
    List index out of range fix "%"

--- Day 8/Caesar Cipher 1/main.py
+++ Day 8/Caesar Cipher 1/main.py
@@ -3,15 +3,22 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+alphabet_len = len(alphabet)
 
 
 # TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
-
+def encrypt(original_text, shift_amount):
+    encrypt_text = ""
 # TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
 #  by the shift amount and print the encrypted text.
-
+    for text in original_text:
+        x = alphabet.index(text)
+        index_num = (x + shift_amount) % alphabet_len
+        encrypt_text += alphabet[index_num]
+    print(f"Here's the encryptd result: {encrypt_text}")
 # TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
 
 # TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
 #  message.
 
+encrypt(text, shift)
--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -6,3 +6,43 @@ def greet_with_name(name):
 
 
 greet_with_name("Jack Bauer")
+
+
+Functions with more than 1 input
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+
+# greet_with("Semin", "Seoul")
+
+greet_with(location="Seoul", name="Semin")
+
+
+# Love Calculator
+# def calculate_love_score(name1, name2):
+#
+#     true_list = ["t", "r", "u", "e"]
+#     love_list = ["l", "o", "v", "e"]
+#
+#     true_count = 0
+#     love_count = 0
+#
+#     for name in name1:
+#         if name in true_list:
+#             true_count += 1
+#         if name in love_list:
+#             love_count += 1
+#
+#     for name in name2:
+#         if name in true_list:
+#             true_count += 1
+#         if name in love_list:
+#             love_count += 1
+#
+#     love_score = str(true_count) + str(love_count)
+#     print(f"Love Score = {love_score}")
+#
+#
+# calculate_love_score(name1="Brad Pitt".lower(), name2="Jennifer Aniston".lower())

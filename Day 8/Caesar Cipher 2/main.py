a92ea938356cf3026ec927195ee8ded8adabf64b
TechnoOrc <technoorc@gmail.com>
2026-06-09 06:49:40 +0900

    Day8 : Functions with Inputs
    #inputs
    #Arguments
    #Parameters
    
    #Love Calculator
    #Caesar Cipher 3
    
    List index() Method
    List index out of range fix "%"

--- Day 8/Caesar Cipher 2/main.py
+++ Day 8/Caesar Cipher 2/main.py
@@ -11,16 +11,17 @@ shift = int(input("Type the shift number:\n"))
 # TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
 #  Use the value of the user chosen 'direction' variable to determine which functionality to use.
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    ouput_text = ""
+    if encode_or_decode == 'encode':
+        shift_amount *= -1
+
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position = alphabet.index(letter) - shift_amount
         shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
-    print(f"Here is the encoded result: {cipher_text}")
+        ouput_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {ouput_text}")
 
 
-encrypt(original_text=text, shift_amount=shift)
-
-
-
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
